52e3b935bcdf9b95f0b25afb225fee844ab33d80
achu2511 <achyuth2511@gmail.com>
2024-06-21 04:21:13 +0530

    first commit

---
+++ website/static/index.js
@@ -0,0 +1,12 @@
+const deleteNote=(noteId)=>{
+    fetch('/delete-note',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({noteId:noteId}),
+    }
+    ).then((_res)=>{
+        window.location.href="/";
+    });
+}
